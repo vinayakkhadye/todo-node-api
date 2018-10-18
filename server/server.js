4ec4a171f20ea8511ea8ff1ee2042fb834d12ad2
vinayak khadye <vinayak.khadye@v2solutions.com>
2018-10-18 14:04:51 +0530

    added tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -24,6 +24,12 @@ app.post('/todos',(req, res)=>{
     })
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send(todos);
+    }).catch(err => res.status(400).send(err));
+})
+
 app.listen(PORT,()=>{
     console.log(`server started on port ${PORT}`);
 });
