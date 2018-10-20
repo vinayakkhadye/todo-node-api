28efe75c793f83ec2eeb40ce2eaf08d79e97dac8
vinayak khadye <vinayak.khadye@v2solutions.com>
2018-10-20 09:19:11 +0530

    mongoose queries

--- db/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/TodoApp';
-mongoose.connect(url,{useNewUrlParser:true});
-
-module.exports={mongoose}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//console.log(process.env);
-const PORT = 3000 ;
-const {mongoose} = require('../db/mongoose');
-const {Todo} = require('../db/models/Todo');
-const {User} = require('../db/models/User');
+const {ObjectID} = require('mongodb');
+
+const PORT = process.env.PORT || 3000 ;
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./db/models/Todo');
+const {User} = require('./db/models/User');
 
 var app = express();
 
@@ -30,10 +31,31 @@ app.get('/todos',(req,res)=>{
     }).catch(err => res.status(400).send(err));
 })
 
+app.get('/todos/:id',(req,res)=>{
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+       return res.status(404).send({error:'id is not valid'});
+    }
+    Todo.findById(id).then((doc)=>{
+            if(doc) {
+                return res.status(200).send(doc);
+        }
+        else {
+            return res.status(404).send({error : 'id not found in db'});
+        }
+    }
+    ).catch((err)=>{
+        return res.status(404).send(err);
+    });
+
+});
+
 app.listen(PORT,()=>{
     console.log(`server started on port ${PORT}`);
 });
 
 module.exports={app};
 
-
+process.on("uncaughtException",(err)=>{
+    console.log(err);
+});
